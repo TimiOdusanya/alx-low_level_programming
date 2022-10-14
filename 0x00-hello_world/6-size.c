f9cbe3b362b870789e4b920afe5c69f37de73db9
TimiOdusanya <timmycruz36@gmail.com>
2022-10-14 14:13:41 -0700

    Added to git

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Description: Function that prints the size of data types
+ *
+ * Return: Success returns 0
+ */
+int main(void)
+{
+	printf("Size of a char: %d byte(s)\n", (int) sizeof(char));
+	printf("Size of an int: %d byte(s)\n", (int) sizeof(int));
+	printf("Size of a long int: %d byte(s)\n", (int) sizeof(long int));
+	printf("Size of a long long int: %d byte(s)\n", (int) sizeof(long long int));
+	printf("Size of a float: %d byte(s)\n", (int) sizeof(float));
+	return (0);
