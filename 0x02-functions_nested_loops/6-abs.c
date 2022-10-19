515794e58e702d6194c048c6d6dc61376eaa0db3
TimiOdusanya <timmycruz36@gmail.com>
2022-10-19 12:40:37 -0700

    Added to git

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _abs - Absolute value of an int n
+ * @n: Operand 1
+ * Description: ABsolute value of an int
+ *
+ * Return: Always n
+ */
+int _abs(int n)
+{
+	if (n > 0)
+	{
+		return (n);
+	}
+	else if (n == 0)
+	{
+		return (n);
+	}
+	else
+	{
+		return (-n);
+	}
+}
