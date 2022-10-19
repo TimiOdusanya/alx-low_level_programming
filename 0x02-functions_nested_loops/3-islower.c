2b09da80d5bb1d297aa2fcde3b7160807ba8db1c
TimiOdusanya <timmycruz36@gmail.com>
2022-10-19 09:23:04 -0700

    Added to git

---
+++ 0x02-functions_nested_loops/3-islower.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * _islower - Checks for lowercase characters
+ *@c: Operand 1
+ * Description: Checks for lowercase characters
+ *
+ * Return: Always 0
+ */
+int _islower(int c)
+{
+	return (c >= 'a' && c <= 'z');
+}
