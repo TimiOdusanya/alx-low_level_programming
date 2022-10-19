4355fe71b66d4ac352e1b093eb6ff621fda130ed
TimiOdusanya <timmycruz36@gmail.com>
2022-10-19 08:17:54 -0700

    Added to git.

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,15 @@
  */
 int main(void)
 {
-	char text[8] = "_putchar";
-
-
-	for (int i = 0; i < 8; i++)
-	{
-		_putchar(text[i]);
-	}
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
+
 	return (0);
 }
