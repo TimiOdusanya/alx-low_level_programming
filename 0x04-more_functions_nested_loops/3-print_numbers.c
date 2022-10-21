3993fab63ec008ccbb505e72398dd8a09344f928
TimiOdusanya <timmycruz36@gmail.com>
2022-10-21 08:42:34 -0700

    Added new changes to git

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers - prints the numbers, from 0 to 9
+ *
+ * Description: prints the numbers, from 0 to 9 followed by a new line
+ * Return: null
+ */
+
+
+void print_numbers(void)
+{
+	int i = 0;
+
+	for (i = 0; i < 10; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
