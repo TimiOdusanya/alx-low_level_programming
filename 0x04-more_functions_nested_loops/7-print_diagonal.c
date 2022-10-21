41357eac9af1bd13d27202462ac0c4dc061da6e9
TimiOdusanya <timmycruz36@gmail.com>
2022-10-21 16:14:57 -0700

    Added to git

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -17,9 +17,9 @@ void print_diagonal(int n)
 	{
 		int i, j;
 
-		for (i = 1; i <= n; i++)
+		for (i = 0; i <= n; i++)
 		{
-			for (j = 1; j < n; j++)
+			for (j = 0; j < n; j++)
 			{
 				if (j == i)
 					_putchar('\\');
