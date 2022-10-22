7769e7314c881e2f03fefb288650d8ab4b714636
TimiOdusanya <timmycruz36@gmail.com>
2022-10-21 17:09:27 -0700

    Added 9-fizz_buzz to git

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+/**
+ * main - Ap rogram that prints out FizzBuzz
+ *
+ * Description: Prints FIzz if 3 annd Buzz if 5 and FizzBuzz if 15 till 100
+ *
+ * Return: 0 if successful
+ */
+
+int main(void)
+{
+	int i;
+
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else if (i % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (i % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else
+		{
+			printf("%d ", i);
+		}
+	}
+	printf("\n");
+	return (0);
+}
