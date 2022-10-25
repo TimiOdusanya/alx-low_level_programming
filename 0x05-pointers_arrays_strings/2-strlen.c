d82a0159ae60b52ac53b4c4145a0c4b4fe6dbb06
TimiOdusanya <timmycruz36@gmail.com>
2022-10-25 12:17:30 -0700

    Added 2-strlen.c to git

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _strlen -  a function that returns the length of a string.
+ * @s: Operand 1
+ *
+ * Return: always 0 if successful
+ */
+int _strlen(char *s)
+{
+	_strlen(s);
+}
