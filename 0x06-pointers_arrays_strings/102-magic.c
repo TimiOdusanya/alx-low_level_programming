f60364aae7f1991c2a8be509a0b232a7724f5718
TimiOdusanya <timmycruz36@gmail.com>
2022-10-28 15:54:46 -0700

    Added 102-magic.c changes to git

--- 0x06-pointers_arrays_strings/102-magic.c
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -1,10 +1,5 @@
 #include <stdio.h>
 
-/**
- * main - Main function
- *
- * Return: Always 0
- */
 int main(void)
 {
 	int n;
@@ -22,7 +17,6 @@ int main(void)
 	 * - you are not allowed to code anything else than this line of code
 	 */
 	*(p + 5) = 98;
-	/* ...so that this prints 98\n */
 	printf("a[2] = %d\n", a[2]);
 	return (0);
 }
